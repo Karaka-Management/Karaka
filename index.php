fd50335411741cebf285a54328d06def1eb78727
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-01-26 12:59:37 +0100

    Copyright date fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * @package    TBD
  * @author     OMS Development Team <dev@oms.com>
  * @author     Dennis Eichhorn <d.eichhorn@oms.com>
- * @copyright  2013 Dennis Eichhorn
+ * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
  * @link       http://orange-management.com
