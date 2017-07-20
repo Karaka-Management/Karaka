d863a2fd186290cf294a7568f302805169abfe30
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-20 18:10:47 +0200

    Remove @author docblock

--- config.php
+++ config.php
@@ -7,7 +7,6 @@
  * @category   TBD
  * @package    TBD
  * @author     OMS Development Team <dev@oms.com>
- * @author     Dennis Eichhorn <d.eichhorn@oms.com>
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
