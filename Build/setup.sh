add008f9a6c430b1cd4490e798a82dd1634723d7
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-11-05 14:40:08 +0100

    Remove unused tools

--- Build/setup.sh
+++ Build/setup.sh
@@ -69,8 +69,6 @@ if [ ! -d "$TOOLS_PATH" ]; then
     wget -nc https://github.com/Halleck45/PhpMetrics/raw/master/build/phpmetrics.phar
     wget -nc http://phpdoc.org/phpDocumentor.phar
     wget -nc http://static.pdepend.org/php/latest/pdepend.phar
-    wget -nc https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-    wget -nc http://static.phpmd.org/php/latest/phpmd.phar
     wget -nc https://phar.phpunit.de/phpcpd.phar
     wget -nc http://dl.google.com/closure-compiler/compiler-latest.tar.gz
     tar -zxvf compiler-latest.tar.gz
