e399150f610ce129a0e36859ddb7e25d3bdeec5b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-11-16 11:32:37 +0100

    Test sphere

--- Tests/PHPUnit/phpOMS/Math/Geometry/Shape/D3/SphereTest.php
+++ Tests/PHPUnit/phpOMS/Math/Geometry/Shape/D3/SphereTest.php
@@ -21,8 +21,22 @@ use phpOMS\Math\Geometry\Shape\D3\Sphere;
 
 class SphereTest extends \PHPUnit\Framework\TestCase
 {
-    public function testPlaceholder()
+    public function testSphere()
     {
-        self::markTestIncomplete();
+        $sphere = new Sphere(3);
+        self::assertEquals(113.1, $sphere->getVolume(), '', 0.1);
+        self::assertEquals(113.1, $sphere->getSurface(), '', 0.1);
+    }
+
+    public function testGetBy()
+    {
+        $sphere = Sphere::byRadius(3);
+        self::assertEquals(3, $sphere->getRadius(), '', 0.1);
+
+        $sphere = Sphere::byVolume(4);
+        self::assertEquals(4, $sphere->getVolume(), '', 0.1);
+
+        $sphere = Sphere::bySurface(5);
+        self::assertEquals(5, $sphere->getSurface(), '', 0.1);
     }
 }
