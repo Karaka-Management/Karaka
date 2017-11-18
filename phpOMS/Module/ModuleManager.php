df2d5549c5bab0eb21779e8e5fc4e8d3a1c0c70f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-11-18 16:52:34 +0100

    Fix providing install

--- phpOMS/Module/ModuleManager.php
+++ phpOMS/Module/ModuleManager.php
@@ -542,8 +542,7 @@ class ModuleManager
     {
         if (file_exists($this->modulePath . '/' . $from . '/Admin/Install/' . $for . '.php')) {
             $class = '\\Modules\\' . $from . '\\Admin\\Install\\' . $for;
-            /** @var $class InstallerAbstract */
-            $class::install($this->modulePath, $this->app->dbPool, $this->loadInfo($from));
+            $class::install($this->modulePath, $this->app->dbPool);
         }
     }
 
