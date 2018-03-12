c34f3014e3c2464b03b891a0ae7b4552a560a942
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-12 14:54:54 +0100

    Update WebApplication.php

--- Install/WebApplication.php
+++ Install/WebApplication.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7.1
  *
- * @package    TBD
+ * @package    Install
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
@@ -46,7 +46,7 @@ use phpOMS\Module\ModuleManager;
 /**
  * Application class.
  *
- * @package    Framework
+ * @package    Install
  * @license    OMS License 1.0
  * @link       http://website.orange-management.de
  * @since      1.0.0
