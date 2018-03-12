6b68b195f9f1d849eb36ec3fa74eeccf91f3aec1
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-12 15:12:10 +0100

    Update ConsoleApplication.php

--- Install/ConsoleApplication.php
+++ Install/ConsoleApplication.php
@@ -50,8 +50,125 @@ use phpOMS\Module\ModuleManager;
  */
 class ConsoleApplication extends ApplicationAbstract
 {
-    public function install(Request $request, Response $response)
+    /**
+     * Constructor.
+     *
+     * @param array $config Core config
+     *
+     * @since  1.0.0
+     */
+    public function __construct(array $config)
     {
+        $this->setupHandlers();
+
+        $this->logger = FileLogger::getInstance($config['log']['file']['path'], false);
+        $this->config = $config;
+        $request      = $this->initRequest($config['page']['root'], $config['language'][0]);
+        $response     = $this->initResponse($request, $config['language']);
+
+        UriFactory::setupUriBuilder($request->getUri());
+
+        $this->run($request, $response);
+        $response->getHeader()->push();
+
+        echo $response->getBody();
+    }
+    
+    /**
+     * Setup general handlers for the application.
+     *
+     * @return void
+     *
+     * @since  1.0.0
+     */
+    private function setupHandlers() /* : void */
+    {
+        set_exception_handler(['\phpOMS\UnhandledHandler', 'exceptionHandler']);
+        set_error_handler(['\phpOMS\UnhandledHandler', 'errorHandler']);
+        register_shutdown_function(['\phpOMS\UnhandledHandler', 'shutdownHandler']);
+    }
+    
+    /**
+     * Initialize current application request
+     *
+     * @param string $rootPath Web root path
+     * @param string $language Fallback language
+     *
+     * @return Request Initial client request
+     *
+     * @since  1.0.0
+     */
+    private function initRequest(string $rootPath, string $language) : Request
+    {
+        $request     = Request::createFromSuperglobals();
+        $subDirDepth = substr_count($rootPath, '/');
+
+        $request->createRequestHashs($subDirDepth);
+        $request->getUri()->setRootPath($rootPath);
+        UriFactory::setupUriBuilder($request->getUri());
+
+        $langCode = strtolower($request->getUri()->getPathElement(0));
+        $request->getHeader()->getL11n()->setLanguage(
+            empty($langCode) || !ISO639x1Enum::isValidValue($langCode) ? $language : $langCode
+        );
+        UriFactory::setQuery('/lang', $request->getHeader()->getL11n()->getLanguage());
+
+        return $request;
+    }
+
+    /**
+     * Initialize basic response
+     *
+     * @param Request $request Client request
+     * @param array $languages Supported languages
+     *
+     * @return Response Initial client request
+     *
+     * @since  1.0.0
+     */
+    private function initResponse(Request $request, array $languages) : Response
+    {
+        $response = new Response(new Localization());
+        $response->getHeader()->set('content-type', 'text/html; charset=utf-8');
+        $response->getHeader()->set('x-xss-protection', '1; mode=block');
+        $response->getHeader()->set('x-content-type-options', 'nosniff');
+        $response->getHeader()->set('x-frame-options', 'SAMEORIGIN');
+        $response->getHeader()->set('referrer-policy', 'same-origin');
+
+        if ($request->isHttps()) {
+            $response->getHeader()->set('strict-transport-security', 'max-age=31536000');
+        }
+
+        $response->getHeader()->getL11n()->setLanguage(!in_array($request->getHeader()->getL11n()->getLanguage(), $languages) ? 'en' : $request->getHeader()->getL11n()->getLanguage());
+
+        return $response;
+    }
+    
+    /**
+     * Rendering backend.
+     *
+     * @param Request  $request  Request
+     * @param Response $response Response
+     *
+     * @return void
+     *
+     * @since  1.0.0
+     */
+    private function run(Request $request, Response $response) /* : void */
+    {
+        $this->dbPool = new DatabasePool();
+        $this->dispatcher = new Dispatcher($this);
+        $this->router = new Router();
+
+        $this->setupRoutes();
+
+        $this->dbPool->create('admin', $this->config['db']['core']['masters']['admin']);
+        DataMapperAbstract::setConnection($this->dbPool->get());
+
+        $response->getHeader()->set('content-language', $response->getHeader()->getL11n()->getLanguage(), true);
+        UriFactory::setQuery('/lang', $response->getHeader()->getL11n()->getLanguage());
+
+        $this->dispatcher->dispatch($this->router->route($request), $request, $response);
     }
     
     public function loadSetupFile(string $path) : array
