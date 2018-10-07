1d9d0ed452da329c74c680fff5c00b182c56ffc5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-10-07 20:27:03 +0200

    Test fixes

--- tests/Web/Backend/BackendViewTest.php
+++ tests/Web/Backend/BackendViewTest.php
@@ -16,6 +16,7 @@ namespace tests\Web\Backend;
 require_once __DIR__ . '/../../../phpOMS/Autoloader.php';
 
 use Web\Backend\BackendView;
+use Modules\Profile\Models\ProfileMapper;
 
 class BackendViewTest extends \PHPUnit\Framework\TestCase
 {
