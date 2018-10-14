0677968e8384f4f0011be47e8436652e41f0ca56
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-10-14 22:29:34 +0200

    Fix unit tests

---
+++ Web/Api/Hooks.php
@@ -0,0 +1,7 @@
+<?php return [
+    'some-hook-id-name:here' => [
+        0 => '\some\function\to\call:functionname',
+        1 => '\some\function\to\call:functionname',
+        2 => '\some\function\to\call:functionname',
+    ],
+];
