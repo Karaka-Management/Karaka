6cabef87aa04197de2b2c8342f64ca60f95fb0da
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-11-30 21:05:45 +0100

    Force logging output

--- Install/tests/Bootstrap.php
+++ Install/tests/Bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
 ini_set('memory_limit', '2048M');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/Autoloader.php';
