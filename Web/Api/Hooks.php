9154ce9ce7a7b1cf144b99dfb985b8ef71857950
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-02-14 23:44:00 +0100

    Draft auditor diff

--- Web/Api/Hooks.php
+++ Web/Api/Hooks.php
@@ -1,1 +1,17 @@
-<?php return [];
+<?php return [
+    'POST:Module:.*?\-create' => [
+        'callback' => [
+            0 => '\Modules\Auditor\Controller\ApiController:apiLogCreate',
+        ],
+    ],
+    'POST:Module:.*?\-update' => [
+        'callback' => [
+            0 => '\Modules\Auditor\Controller\ApiController:apiLogUpdate',
+        ],
+    ],
+    'POST:Module:.*?\-delete' => [
+        'callback' => [
+            0 => '\Modules\Auditor\Controller\ApiController:apiLogDelete',
+        ],
+    ],
+];
