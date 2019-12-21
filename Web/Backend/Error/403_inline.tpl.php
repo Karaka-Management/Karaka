8fb4026cf44fe34b3ffe8900b30e6f0b46d6bd9a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-12-21 12:22:14 +0100

    add error images

--- Web/Backend/Error/403_inline.tpl.php
+++ Web/Backend/Error/403_inline.tpl.php
@@ -1,1 +1,15 @@
-Inline <?= $this->getHtml('403Message', 0); ?>
+<?php
+
+use phpOMS\Uri\UriFactory;
+?>
+<div class="centerText box">
+    <div>You don't have the required permission to access this content.</div>
+</div>
+
+<div class="centerText box">
+    <div><img src="<?= UriFactory::build('Web/Backend/img/inline_403.svg') ?>" width="30%"></div>
+</div>
+
+<div class="centerText box">
+    <div>Please contact an admin if you think this is an unexpected error.</div>
+</div>
---
+++ Web/Backend/Error/404.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+use phpOMS\Uri\UriFactory;
+?>
+<div class="centerText box">
+    <div>This content doesn't exist.</div>
+</div>
+
+<div class="centerText box">
+    <div><img src="<?= UriFactory::build('Web/Backend/img/inline_404.svg') ?>" width="30%"></div>
+</div>
+
+<div class="centerText box">
+    <div>Please contact an admin if you think this is an unexpected error.</div>
+</div>
