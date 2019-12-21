5c293e8410e365d62a8596218995302f25a94f08
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-12-21 21:40:28 +0100

    stricter typehint

--- Web/E500/Application.php
+++ Web/E500/Application.php
@@ -38,10 +38,10 @@ final class Application
     /**
      * WebApplication.
      *
-     * @var   null|WebApplication
+     * @var   WebApplication
      * @since 1.0.0
      */
-    private ?WebApplication $app = null;
+    private WebApplication $app;
 
     /**
      * Temp config.
