29bb175945d0884397b1892d93e5d556ec121887
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-06 21:03:56 +0100

    fix phpcs

--- Install/InstallAbstract.php
+++ Install/InstallAbstract.php
@@ -17,10 +17,15 @@ namespace Install;
 use Model\Settings;
 
 use Modules\Admin\Controller\ApiController;
+use Modules\Admin\Models\Account;
+use Modules\Admin\Models\AccountMapper;
+use Modules\Admin\Models\Group;
+use Modules\Admin\Models\GroupMapper;
 use Modules\Media\Models\Collection;
 use Modules\Media\Models\CollectionMapper;
 use Modules\Organization\Models\Status;
 use Modules\Organization\Models\UnitMapper;
+
 use phpOMS\Account\AccountStatus;
 use phpOMS\Account\AccountType;
 use phpOMS\Account\GroupStatus;
@@ -29,10 +34,8 @@ use phpOMS\ApplicationAbstract;
 use phpOMS\DataStorage\Database\Connection\ConnectionAbstract;
 use phpOMS\DataStorage\Database\Connection\ConnectionFactory;
 use phpOMS\DataStorage\Database\DatabasePool;
-
 use phpOMS\DataStorage\Database\Schema\Builder as SchemaBuilder;
 use phpOMS\Message\Http\Request;
-
 use phpOMS\Message\Http\Response;
 use phpOMS\Message\RequestAbstract;
 use phpOMS\Module\ModuleManager;
@@ -131,7 +134,7 @@ abstract class InstallAbstract extends ApplicationAbstract
         return ConnectionFactory::create([
             'db'       => (string) $request->getData('dbtype'),
             'host'     => (string) $request->getData('dbhost'),
-            'port'     => (int)    $request->getData('dbport'),
+            'port'     => (int) $request->getData('dbport'),
             'prefix'   => (string) $request->getData('dbprefix'),
             'database' => (string) $request->getData('dbname'),
             'login'    => (string) $request->getData('schemauser'),
@@ -336,14 +339,17 @@ abstract class InstallAbstract extends ApplicationAbstract
      */
     protected static function installMainGroups(ConnectionAbstract $db) : void
     {
-        $date = new \DateTime('NOW', new \DateTimeZone('UTC'));
+        $guest = new Group('guest');
+        $guest->setStatus(GroupStatus::ACTIVE);
+        GroupMapper::create($guest);
 
-        $db->con->prepare(
-            'INSERT INTO `' . $db->prefix . 'group` (`group_name`, `group_desc`, `group_status`, `group_created`) VALUES
-                (\'guest\', NULL, ' . GroupStatus::ACTIVE . ', \'' . $date->format('Y-m-d H:i:s') . '\'),
-                (\'user\', NULL, ' . GroupStatus::ACTIVE . ', \'' . $date->format('Y-m-d H:i:s') . '\'),
-                (\'admin\', NULL, ' . GroupStatus::ACTIVE . ', \'' . $date->format('Y-m-d H:i:s') . '\');'
-        )->execute();
+        $user = new Group('user');
+        $user->setStatus(GroupStatus::ACTIVE);
+        GroupMapper::create($user);
+
+        $admin = new Group('admin');
+        $admin->setStatus(GroupStatus::ACTIVE);
+        GroupMapper::create($admin);
     }
 
     /**
@@ -375,7 +381,6 @@ abstract class InstallAbstract extends ApplicationAbstract
      */
     protected static function installUsers(RequestAbstract $request, ConnectionAbstract $db) : void
     {
-        self::installUserLocalization($db);
         self::installMainUser($request, $db);
     }
 
@@ -408,27 +413,6 @@ abstract class InstallAbstract extends ApplicationAbstract
     }
 
     /**
-     * Setup global localization
-     *
-     * @param ConnectionAbstract $db Database connection
-     *
-     * @return void
-     *
-     * @since 1.0.0
-     */
-    protected static function installUserLocalization(ConnectionAbstract $db) : void
-    {
-        /**
-         * @todo Orange-Management/Orange-Management#47
-         *  It should be possible to define the localization in the install script
-         */
-        $db->con->prepare(
-            'INSERT INTO `' . $db->prefix . 'l11n` (`l11n_country`, `l11n_language`, `l11n_currency`, `l11n_number_thousand`, `l11n_number_decimal`, `l11n_angle`, `l11n_temperature`, `l11n_weight_very_light`, `l11n_weight_light`, `l11n_weight_medium`, `l11n_weight_heavy`, `l11n_weight_very_heavy`, `l11n_speed_very_slow`, `l11n_speed_slow`, `l11n_speed_medium`, `l11n_speed_fast`, `l11n_speed_very_fast`, `l11n_speed_sea`, `l11n_length_very_short`, `l11n_length_short`, `l11n_length_medium`, `l11n_length_long`, `l11n_length_very_long`, `l11n_length_sea`, `l11n_area_very_small`, `l11n_area_small`, `l11n_area_medium`, `l11n_area_large`, `l11n_area_very_large`, `l11n_volume_very_small`, `l11n_volume_small`, `l11n_volume_medium`, `l11n_volume_large`, `l11n_volume_very_large`, `l11n_volume_teaspoon`, `l11n_volume_tablespoon`, `l11n_volume_glass`) VALUES
-                (\'DE\', \'EN\', \'EUR\', \',\', \'.\', \'degree\', \'celsius\', \'mg\', \'g\', \'kg\', \'t\', \'t\', \'ms\', \'ms\', \'kph\', \'kph\', \'kph\', \'knot\', \'mm\', \'cm\', \'m\', \'km\', \'km\', \'mile\', \'mm\', \'cm\', \'m\', \'km\', \'km\', \'ml\', \'cl\', \'l\', \'l\', \'l\', \'metric teaspoon\', \'metric tablespoon\', \'metric glass\'), (\'DE\', \'EN\', \'EUR\', \',\', \'.\', \'degree\', \'celsius\', \'mg\', \'g\', \'kg\', \'t\', \'t\', \'ms\', \'ms\', \'kph\', \'kph\', \'kph\', \'knot\', \'mm\', \'cm\', \'m\', \'km\', \'km\', \'mile\', \'mm\', \'cm\', \'m\', \'km\', \'km\', \'ml\', \'cl\', \'l\', \'l\', \'l\', \'metric teaspoon\', \'metric tablespoon\', \'metric glass\');'
-        )->execute();
-    }
-
-    /**
      * Setup root user in database
      *
      * @param RequestAbstract    $request Request
@@ -440,28 +424,19 @@ abstract class InstallAbstract extends ApplicationAbstract
      */
     protected static function installMainUser(RequestAbstract $request, ConnectionAbstract $db) : void
     {
-        $date = new \DateTime('NOW', new \DateTimeZone('UTC'));
+        $account = new Account();
+        $account->setStatus(AccountStatus::ACTIVE);
+        $account->setType(AccountType::USER);
+        $account->setName((string) $request->getData('adminname'));
+        $account->setName1((string) $request->getData('adminname'));
+        $account->generatePassword((string) $request->getData('adminpassword'));
+        $account->setEmail((string) $request->getData('adminemail'));
 
-        $stmt = $db->con->prepare(
-            'INSERT INTO `' . $db->prefix . 'account` (`account_status`, `account_type`, `account_login`, `account_name1`, `account_name2`, `account_name3`, `account_password`, `account_email`, `account_tries`, `account_lactive`, `account_localization`, `account_created_at`) VALUES
-                (' . AccountStatus::ACTIVE . ', ' . AccountType::USER . ', :adminlogin, :adminname, \'\', \'\', :adminpassword, :adminemail, 5, \'' . $date->format('Y-m-d H:i:s') . '\', 2, \'' . $date->format('Y-m-d H:i:s') . '\');'
-        );
-
-        $adminlogin    = (string) $request->getData('adminname');
-        $adminname     = $adminlogin;
-        $adminpassword = \password_hash((string) $request->getData('adminpassword'), \PASSWORD_DEFAULT);
-        $adminemail    = (string) $request->getData('adminemail');
-
-        $stmt->bindParam(':adminlogin', $adminlogin);
-        $stmt->bindParam(':adminname', $adminname);
-        $stmt->bindParam(':adminpassword', $adminpassword);
-        $stmt->bindParam(':adminemail', $adminemail);
-
-        $stmt->execute();
+        AccountMapper::create($account);
 
         $db->con->prepare(
             'INSERT INTO `' . $db->prefix . 'account_group` (`account_group_group`, `account_group_account`) VALUES
-                (3, 1);'
+                (3, ' . $account->getId() . ');'
         )->execute();
     }
 
@@ -499,42 +474,8 @@ abstract class InstallAbstract extends ApplicationAbstract
                 (1000000019, NULL, \'country\', \'DE\', NULL),
                 (1000000020, NULL, \'language\', \'en\', NULL),
                 (1000000021, NULL, \'timezone\', \'Europe/Berlin\', NULL),
-                (1000000022, NULL, \'temperature\', \'celsius\', NULL),
                 (1000000023, NULL, \'currency\', \'USD\', NULL),
-                (1000000025, NULL, \'mail_admin\', \'mail@admin.com\', NULL),
-                (1000000026, NULL, \'login_name\', \'1\', NULL),
-                (1000000027, NULL, \'decimal_point\', \'.\', NULL),
-                (1000000028, NULL, \'thousands_sep\', \',\', NULL),
-                (1000001001, NULL, \'weight_very_light\', \',\', NULL),
-                (1000001002, NULL, \'weight_light\', \',\', NULL),
-                (1000001003, NULL, \'weight_medium\', \',\', NULL),
-                (1000001004, NULL, \'weight_heavy\', \',\', NULL),
-                (1000001005, NULL, \'weight_very_heavy\', \',\', NULL),
-                (1000002001, NULL, \'speed_very_slow\', \',\', NULL),
-                (1000002002, NULL, \'speed_slow\', \',\', NULL),
-                (1000002003, NULL, \'speed_medium\', \',\', NULL),
-                (1000002004, NULL, \'speed_fast\', \',\', NULL),
-                (1000002005, NULL, \'speed_very_fast\', \',\', NULL),
-                (1000002006, NULL, \'speed_sea\', \',\', NULL),
-                (1000003001, NULL, \'length_very_short\', \',\', NULL),
-                (1000003002, NULL, \'length_short\', \',\', NULL),
-                (1000003003, NULL, \'length_medium\', \',\', NULL),
-                (1000003004, NULL, \'length_fast\', \',\', NULL),
-                (1000003005, NULL, \'length_very_fast\', \',\', NULL),
-                (1000003006, NULL, \'length_sea\', \',\', NULL),
-                (1000004001, NULL, \'area_very_small\', \',\', NULL),
-                (1000004002, NULL, \'area_small\', \',\', NULL),
-                (1000004003, NULL, \'area_medium\', \',\', NULL),
-                (1000004004, NULL, \'area_large\', \',\', NULL),
-                (1000004005, NULL, \'area_very_large\', \',\', NULL),
-                (1000005001, NULL, \'volume_very_small\', \',\', NULL),
-                (1000005002, NULL, \'volume_small\', \',\', NULL),
-                (1000005003, NULL, \'volume_medium\', \',\', NULL),
-                (1000005004, NULL, \'volume_large\', \',\', NULL),
-                (1000005005, NULL, \'volume_very_large\', \',\', NULL),
-                (1000005006, NULL, \'volume_teaspoon\', \',\', NULL),
-                (1000005007, NULL, \'volume_tablespoon\', \',\', NULL),
-                (1000005008, NULL, \'volume_glass\', \',\', NULL)'
+                (1000000025, NULL, \'mail_admin\', \'mail@admin.com\', NULL)'
         )->execute();
     }
 }
