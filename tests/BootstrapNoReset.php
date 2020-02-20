506553285f713d2cd6319b538ab42c6df9614bed
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-20 18:34:45 +0100

    Add webserver for tests

--- tests/BootstrapNoReset.php
+++ tests/BootstrapNoReset.php
@@ -304,3 +304,64 @@ $GLOBALS['dbpool']->create('insert', $CONFIG['db']['core']['masters']['insert'])
 $GLOBALS['dbpool']->create('schema', $CONFIG['db']['core']['masters']['schema']);
 
 DataMapperAbstract::setConnection($GLOBALS['dbpool']->get());
+
+function phpServe()
+{
+    // OS detection
+    $isWindows = \stristr(\php_uname('s'), 'Windows') !== false;
+
+    // Command that starts the built-in web server
+    if ($isWindows) {
+        $command = \sprintf(
+            'wmic process call create "php -S %s:%d -t %s" | find "ProcessId"',
+            WEB_SERVER_HOST,
+            WEB_SERVER_PORT,
+            __DIR__ . '/../' . WEB_SERVER_DOCROOT
+        );
+
+        $killCommand = 'taskkill /f /pid ';
+    } else {
+        $command = \sprintf(
+            'php -S %s:%d -t %s >/dev/null 2>&1 & echo $!',
+            WEB_SERVER_HOST,
+            WEB_SERVER_PORT,
+            WEB_SERVER_DOCROOT
+        );
+
+        $killCommand = 'kill ';
+    }
+
+    // Execute the command and store the process ID
+    $output = [];
+    echo \sprintf('Starting server...') . PHP_EOL;
+    echo \sprintf(' Current directory: %s', \getcwd()) . PHP_EOL;
+    echo \sprintf(' %s', $command);
+    \exec($command, $output);
+
+    // Get PID
+    if ($isWindows) {
+        $pid = \explode('=', $output[0]);
+        $pid = \str_replace(' ', '', $pid[1]);
+        $pid = \str_replace(';', '', $pid);
+    } else {
+        $pid = (int) $output[0];
+    }
+
+    // Log
+    echo \sprintf(
+        ' %s - Web server started on %s:%d with PID %d',
+        date('r'),
+        WEB_SERVER_HOST,
+        WEB_SERVER_PORT,
+        $pid
+    ) . PHP_EOL;
+
+    // Kill the web server when the process ends
+    \register_shutdown_function(function() use ($killCommand, $pid) {
+        echo PHP_EOL . \sprintf('Stopping server...') . PHP_EOL;
+        echo \sprintf(' %s - Killing process with ID %d', \date('r'), $pid) . PHP_EOL;
+        \exec($killCommand . $pid);
+    });
+}
+
+phpServe();
