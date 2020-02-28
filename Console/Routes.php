eb4cac7be9eef43f2877ee415d71cb790d673c4b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-28 20:52:53 +0100

    ignore installed files

--- Console/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-return [
-    '^$' => [
-        0 => [
-            'dest' => '\Modules\Admin\ConsoleController:viewEmptyCommand',
-            'verb' => 16,
-        ],
-    ],
-];
