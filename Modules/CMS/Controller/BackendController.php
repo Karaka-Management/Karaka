dd33149d39463e073260f6fdaddebbd999fd306c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-04-13 10:42:50 +0200

    added cms module

---
+++ Modules/CMS/Controller/BackendController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\CMS
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\CMS\Controller;
+
+/**
+ * CMS class.
+ *
+ * @package Modules\CMS
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+final class BackendController extends Controller
+{
+}
