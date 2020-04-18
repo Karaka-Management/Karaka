278dabb3c9163b4d09c2634520df2250b7344d84
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-04-18 22:33:52 +0200

    add click and submit actions

--- Install/Application/Backend/js/global/ActionEvents.js
+++ Install/Application/Backend/js/global/ActionEvents.js
@@ -2,10 +2,15 @@ import { redirectMessage } from '../../../../jsOMS/Model/Message/Redirect.js';
 import { requestAction } from '../../../../jsOMS/Model/Action/Message/Request.js';
 import { reloadButtonAction } from '../../../../jsOMS/Model/Action/Dom/Reload.js';
 import { preventEvent } from '../../../../jsOMS/Model/Action/Event/Prevent.js';
+import { domClickAction } from '../../../../jsOMS/Model/Action/Dom/Click.js';
+import { formSubmitAction } from '../../../../jsOMS/Model/Action/Dom/FormSubmit.js';
 
 export const ACTION_EVENTS = {
     'redirect': redirectMessage, /** global: redirectMessage */
     'message.request': requestAction, /** global: requestAction */
     'dom.reload': reloadButtonAction, /** global: reloadButtonAction */
+    'dom.click': domClickAction, /** global: domClickAction */
+    'form.submit': formSubmitAction, /** global: domClickAction */
     'event.prevent': preventEvent, /** global: preventEvent */
+    'form.submit': formSubmitAction, /** global: formSubmitAction */
 };
