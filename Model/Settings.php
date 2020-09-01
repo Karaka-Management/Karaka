9e2156ca8dcbce5e37e8be1385d2126bd22606c2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-01 20:57:58 +0200

    manual cs fixes

--- Model/Settings.php
+++ Model/Settings.php
@@ -26,25 +26,25 @@ use phpOMS\Stdlib\Base\Enum;
  */
 abstract class Settings extends Enum
 {
-    public const PASSWORD_PATTERN     = 1000000001;
+    public const PASSWORD_PATTERN = 1000000001;
 
-    public const LOGIN_TIMEOUT        = 1000000002;
+    public const LOGIN_TIMEOUT = 1000000002;
 
-    public const PASSWORD_INTERVAL    = 1000000003;
+    public const PASSWORD_INTERVAL = 1000000003;
 
-    public const PASSWORD_HISTORY     = 1000000004;
+    public const PASSWORD_HISTORY = 1000000004;
 
-    public const LOGIN_TRIES          = 1000000005;
+    public const LOGIN_TRIES = 1000000005;
 
-    public const LOGGING_STATUS       = 1000000006;
+    public const LOGGING_STATUS = 1000000006;
 
-    public const LOGGING_PATH         = 1000000007;
+    public const LOGGING_PATH = 1000000007;
 
     public const DEFAULT_ORGANIZATION = 1000000009;
 
-    public const LOGIN_STATUS         = 1000000013;
+    public const LOGIN_STATUS = 1000000013;
 
     public const DEFAULT_LOCALIZATION = 1000000014;
 
-    public const ADMIN_MAIL           = 1000000015;
+    public const ADMIN_MAIL = 1000000015;
 }
