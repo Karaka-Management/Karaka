2d0b8dfee3d596a437f15bcc4807e9a3247cd5f8
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-07 23:01:03 +0200

    add comments

--- Model/Setting.php
+++ Model/Setting.php
@@ -24,23 +24,80 @@ namespace Model;
  */
 class Setting
 {
+    /**
+     * Id
+     *
+     * @var int
+     * @since 1.0.0
+     */
     protected int $id = 0;
 
+    /**
+     * Name
+     *
+     * @var string
+     * @since 1.0.0
+     */
     public string $name = '';
 
+    /**
+     * Content
+     *
+     * @var string
+     * @since 1.0.0
+     */
     public string $content = '';
 
+    /**
+     * Module name
+     *
+     * @var null|string
+     * @since 1.0.0
+     */
     public ?string $module = null;
 
+    /**
+     * Group
+     *
+     * @var null|int
+     * @since 1.0.0
+     */
     public ?int $group = null;
 
+    /**
+     * Account
+     *
+     * @var null|int
+     * @since 1.0.0
+     */
     public ?int $account = null;
 
+    /**
+     * Get id
+     *
+     * @return int
+     *
+     * @since 1.0.0
+     */
     public function getId() : int
     {
         return $this->id;
     }
 
+    /**
+     * Create setting with data
+     *
+     * @param int      $id      Id
+     * @param string   $name    Name
+     * @param string   $content Content
+     * @param string   $module  Module
+     * @param null|int $group   Group
+     * @param null|int $account Account
+     *
+     * @return self
+     *
+     * @since 1.0.0
+     */
     public function with(
         int $id = 0,
         string $name = '',
