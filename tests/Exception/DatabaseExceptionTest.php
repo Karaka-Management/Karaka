de2350f978973b695cd2578b87cd229498886282
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-09 00:19:50 +0200

    fix @covers

--- tests/Exception/DatabaseExceptionTest.php
+++ tests/Exception/DatabaseExceptionTest.php
@@ -23,6 +23,10 @@ use Web\Exception\DatabaseException;
  */
 class DatabaseExceptionTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @covers Web\Exception\DatabaseException
+     * @group framework
+     */
     public function testException() : void
     {
         self::assertInstanceOf(\RuntimeException::class, new DatabaseException(''));
