cbd386ae77b7796e841e3c123984e37e96757f4b
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-02 03:23:14 +0000

    bump

--- tests/Autoloader.php
+++ tests/Autoloader.php
@@ -78,7 +78,7 @@ final class Autoloader
         $class = \strtr($class, '_\\', '//');
 
         if (\stripos($class, 'Web/Backend') !== false || \stripos($class, 'Web/Api') !== false) {
-            $class = \is_dir(__DIR__ . '/Web') ? $class : \str_replace('Web/', 'Karaka/Web/', $class);
+            $class = \is_dir(__DIR__ . '/Web') ? $class : \str_replace('Web/', 'MainRepository/Web/', $class);
         }
 
         $class2 = $class;
